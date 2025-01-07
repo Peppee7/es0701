3629abf699872681c26f517f571c135b9efec9a8
flexmyedit <giuseppesanese07@outlook.it>
2025-01-07 09:50:51 +0100

    bug

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 let elements = document.querySelectorAll(".elem");
 
 for (let i = 0; i < elements.length; i++) {
-    let el = elements[i]
+    let el = elements[i];
     el.classList.add("greenBg");
     el.addEventListener("click", function(e) {
         el.classList.remove("greenBg");
